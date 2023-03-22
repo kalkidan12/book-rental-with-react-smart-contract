88923e31baa409c2e15004b2935ed46aab09943d
kalkidan12 <kalget12@gmail.com>
2023-03-22 21:42:57 +0300

    adding navbar

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<nav className="flex bg-slate-100 justify-between shadow h-[50px] px-5 py-2">
+			<h1 className="text-2xl font-bold text-slate-800">Book Rental</h1>
+			<div className="flex gap-5">
+				<button className="btn bg-blue-300 rounded-md px-4">Sign In</button>
+			</div>
+		</nav>
+	);
+};
+
+export default Header;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "../components/Header/Header";
 
 const HomePage = () => {
-	return <div>Home Page</div>;
+	return (
+		<div className="w-full h-full">
+			<Header />
+		</div>
+	);
 };
 
 export default HomePage;
